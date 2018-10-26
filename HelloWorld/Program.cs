eff084ab8cf75315791f2da3788668be70cd7fdf
Lisa Leu <Lisal@netwealth.com.au>
2018-10-26 14:46:14 +1100

    Change

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {   
-            string name = args[1];
+            string name = args[0];
             Console.WriteLine($"Hello {name}");
         }
     }
